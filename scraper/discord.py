f5367b0ac534cfe41c006c76174138001b7cecc9
arrowed <186848+arrowed@users.noreply.github.com>
2025-03-26 21:49:02 +1100

    Writing secrets to logs is kinda a bad idea

--- scraper/discord.py
+++ scraper/discord.py
@@ -1,5 +1,6 @@
 import requests
 
+
 class DiscordApi:
     def __init__(self, token):
         self.token = token
@@ -9,15 +10,16 @@ class DiscordApi:
         self.email = None
         self.phone = None
 
-        self.HEADERS = {"authorization": self.token, "content-type": "application/json"}
+        self.HEADERS = {"authorization": self.token,
+                        "content-type": "application/json"}
         self._validate()
 
     def _validate(self) -> None:
         """This will check if the discord token works"""
         url = f"https://discord.com/api/users/@me"
-        r = requests.get(url, headers=self.HEADERS)
+        r = requests.get(url, headers=self.HEADERS, timeout=10)
         if r.status_code == 200:
-            print(f"Valid token: {self.token}" )
+            print(f"Valid token: ...{self.token[-4:]}")
             data = r.json()
             self.username = data['username'] + "#" + data['discriminator']
             self.id = data['id']
@@ -30,8 +32,9 @@ class DiscordApi:
     def get_messages(self, channel_id: str, page: int = 0) -> list:
         """It will get 25 messages from a channel"""
         offset = 25 * page
-        url = f"https://discord.com/api/channels/{channel_id}/messages" #/search?offset={offset}"
-        r = requests.get(url, headers=self.HEADERS)
+        # /search?offset={offset}"
+        url = f"https://discord.com/api/channels/{channel_id}/messages"
+        r = requests.get(url, headers=self.HEADERS, timeout=10)
         if r.status_code in [200, 201, 204]:
             return r.json()
         else:
